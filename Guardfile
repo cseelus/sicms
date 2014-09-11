b3d94f929f5b67625f4931f421502878343f4404
Chris Seelus <chris@imeos.com>
2014-09-11 05:13:41 +0200

    Part II of tutorial
    Finish CRUD functionality for pages with code for update + delete
    actions.
    
    Add 'permalink' lambda, and make pages accessible via a link, generated
    from the title.
    
    Add basic styling via a small SASS file.

--- Guardfile
+++ Guardfile
@@ -1,8 +1,5 @@
 guard 'livereload' do
  # sinatra
   watch(%r{.+\.rb})
-  watch(%r{.+\.slim})
-  watch(%r{.+\.sass})
-  watch(%r{.+\.coffee})
-  # watch(%r{views/.+\.(slim|sass|css)})
+  watch(%r{views/.+\.(slim|sass|css|coffee|js)})
 end
--- main.rb
+++ main.rb
@@ -6,19 +6,52 @@ require 'bootstrap-sass'
 require 'redcarpet'
 require 'rack-livereload'
 
+
+# Configuration
+# ---------------------------------------------------------------------
+
+# Mongoid
 configure do
   Mongoid.load!("./mongoid.yml")
 end
-
+# Slim
+Slim::Engine.set_default_options pretty: true
+# Rack
 use Rack::LiveReload
+# Assets
+get('/views/assets/stylesheets/application.css'){ sass :'assets/stylesheets/application' }
+
+
+# Models
+# ---------------------------------------------------------------------
 
 class Page
   include Mongoid::Document
 
   field :title, type: String
   field :content, type: String
+  field :permalink, type: String, default: -> { make_permalink }
+
+  def make_permalink
+    title.parameterize if title
+  end
+  def parameterize(string, sep = '-')
+    # Turn unwanted chars into the separator
+    parameterized_string.gsub!(/[^a-z0-9\-_]+/, sep)
+    unless sep.nil? || sep.empty?
+      re_sep = Regexp.escape(sep)
+      # No more than one of the separator in a row.
+      parameterized_string.gsub!(/#{re_sep}{2,}/, sep)
+      # Remove leading/trailing separator.
+      parameterized_string.gsub!(/^#{re_sep}|#{re_sep}$/, '')
+    end
+    parameterized_string.downcase
+  end
 end
 
+
+# Routes
+# ---------------------------------------------------------------------
 
 get '/pages' do
   @pages = Page.all
@@ -31,6 +64,20 @@ get '/pages/new' do
   slim :new
 end
 
+get '/:permalink' do
+  begin
+    @page = Page.find_by(permalink: params[:permalink])
+  rescue
+    pass
+  end
+  slim :show
+end
+
+get '/pages/:id/edit' do
+  @page = Page.find(params[:id])
+  slim :edit
+end
+
 get '/pages/:id' do
   @page = Page.find(params[:id])
   @title = @page.title
@@ -40,4 +87,20 @@ end
 post '/pages' do
   page = Page.create(params[:page])
   redirect to("pages/#{page.id}")
+end
+
+put '/pages/:id' do
+  page = Page.find(params[:id])
+  page.update_attributes(params[:page])
+  redirect to("/pages/#{page.id}")
+end
+
+get '/pages/delete/:id' do
+  @page = Page.find(params[:id])
+  slim :delete
+end
+
+delete '/pages/:id' do
+  Page.find(params[:id]).destroy
+  redirect to('/pages')
 end
